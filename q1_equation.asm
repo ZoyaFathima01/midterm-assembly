5f1c5d6931629c97da316aa623d6bcf41fb51c2f
ZoyaFathima01 <zoyaafathimaa001@gmail.com>
2025-07-04 15:46:29 -0700

    Create q1_equation.asm

---
+++ q1_equation.asm
@@ -0,0 +1,34 @@
+; var1 = 10
+; var2 = 3
+; var3 = 7
+; result = (10 + 2) / (7 - 3) = 12 / 4 = 3
+
+section .data
+    msg db "Result is: %d", 10, 0
+    var1 dd 10
+    var2 dd 3
+    var3 dd 7
+    result dd 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    mov eax, [var1]      ; eax = var1
+    add eax, 2           ; eax = var1 + 2
+    mov ebx, [var3]
+    sub ebx, [var2]      ; ebx = var3 - var2
+
+    cdq                  ; Sign extend eax into edx:eax
+    idiv ebx             ; eax = eax / ebx
+
+    mov [result], eax    ; Store quotient (result) in memory
+
+    push eax             ; Push result to stack
+    push msg             ; Push format string
+    call printf
+    add esp, 8           ; Clean up stack
+
+    mov eax, 0
+    ret
