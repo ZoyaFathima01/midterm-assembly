1e2af393591f080ce8bbdfdd1db79bbe257b881a
ZoyaFathima01 <zoyaafathimaa001@gmail.com>
2025-07-04 16:25:25 -0700

    Update q1_equation.asm

--- q1_equation.asm
+++ q1_equation.asm
@@ -4,7 +4,7 @@
 ; result = (10 + 2) / (7 - 3) = 12 / 4 = 3
 
 section .data
-    msg db "Result is: %d", 10, 0
+    msg db "Result is: %d", 10, 0     ; format string with newline
     var1 dd 10
     var2 dd 3
     var3 dd 7
@@ -17,19 +17,20 @@ section .text
 main:
     mov eax, [var1]      ; eax = var1
     add eax, 2           ; eax = var1 + 2
-    mov ebx, [var3]
+
+    mov ebx, [var3]      
     sub ebx, [var2]      ; ebx = var3 - var2
 
-    cdq                  ; Sign extend eax into edx:eax
-    idiv ebx             ; eax = eax / ebx
+    cdq                  ; sign extend eax into edx:eax
+    idiv ebx             ; eax = eax / ebx, edx = remainder
 
-    mov [result], eax    ; Store quotient (result) in memory
+    mov [result], eax    ; store quotient in result
 
-    push eax             ; Push result to stack
-    push msg             ; Push format string
-    call printf
-    add esp, 8           ; Clean up stack
+    push eax             ; push quotient for printf
+    push msg             ; push format string
+    call printf          ; call printf
+    add esp, 8           ; clean stack after call
 
-mov eax, 1       ; syscall number for exit
-xor ebx, ebx     ; exit code 0
-int 0x80         ; call Linux kernel to exit
+    mov eax, 1           ; syscall: exit
+    xor ebx, ebx         ; status 0
+    int 0x80             ; exit program
